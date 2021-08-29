a6fb6d0da860a3435c292c21c913bb9e9a218850
Leo Rönnebro <leo@letnh.com>
2021-08-29 12:30:34 +0200

    Modifed keybindings

--- config.def.h
+++ config.def.h
@@ -51,7 +51,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -65,18 +66,29 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *volmutecmd[] = { "amixer", "set", "Master", "toggle", NULL };
+static const char *voldowncmd[] = { "amixer", "set", "Master", "5%-", NULL };
+static const char *volupcmd[] = { "amixer", "set", "Master", "5%+", NULL };
+static const char *micmutecmd[] = { "amixer", "set", "Capture", "toggle", NULL };
+static const char *lockscreencmd[] = { "slock", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             		XK_F1,     spawn,          {.v = volmutecmd } },
+	{ MODKEY,             		XK_F2,     spawn,          {.v = voldowncmd } },
+	{ MODKEY,             		XK_F3,     spawn,          {.v = volupcmd } },
+	{ MODKEY,             		XK_F4,     spawn,          {.v = micmutecmd } },
+	{ MODKEY,                       XK_l,      spawn,          {.v = lockscreencmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ALTKEY,                XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ALTKEY,                XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
